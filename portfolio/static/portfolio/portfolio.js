1174be95cf014c688e86cc2d41d5cf3b39623ae6
matt-harrop <osaintspreserveus@live.com>
2021-04-27 16:57:39 -0700

    Added button to intro that scrolls down to bio section. Added (empty, for now) JS file.

---
+++ portfolio/static/portfolio/portfolio.js
@@ -0,0 +1,5 @@
+// const introScrollButton = document.querySelector('#intro-scroll-button');
+// introScrollButton.addEventListener('click', function() {
+//     console.log('Scroll button pushed.');
+//     document.querySelector('#about').scrollIntoView({behavior: 'smooth'});
+// })
