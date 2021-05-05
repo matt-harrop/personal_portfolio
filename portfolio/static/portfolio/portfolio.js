b91b69e29725fe1fda0c78cd50bd4f8f52229e67
matt-harrop <osaintspreserveus@live.com>
2021-05-05 16:15:51 -0700

    Finished basic navigation menu that hovers to the side of the screen and sticks to its viewport position once it's been scrolled past; still need to optimize it for mobile-size screens.

--- portfolio/static/portfolio/portfolio.js
+++ portfolio/static/portfolio/portfolio.js
@@ -1,5 +1,22 @@
-// const introScrollButton = document.querySelector('#intro-scroll-button');
-// introScrollButton.addEventListener('click', function() {
-//     console.log('Scroll button pushed.');
-//     document.querySelector('#about').scrollIntoView({behavior: 'smooth'});
-// })
+const floatingNavBar = document.querySelector("#floatingNavBar");
+const bioDiv = document.querySelector("#about");
+let previousBioDivY = bioDiv.getBoundingClientRect().y;
+const navBarTopBuffer = 20;
+
+console.log(bioDiv.getBoundingClientRect().y);
+
+document.addEventListener('scroll', function(e) {
+    //Right here, need to check if the navbar is at the top of the viewport and then make it stick there if it is
+    //by changing the position to relative(?) with a buffer from the top.
+
+    if (previousBioDivY > navBarTopBuffer && bioDiv.getBoundingClientRect().y <= navBarTopBuffer) {
+        // alert('Just scrolled past the top of the Bio Div (from above)!');
+        floatingNavBar.classList.add('floating-nav-bar');
+    } else if (previousBioDivY < navBarTopBuffer && bioDiv.getBoundingClientRect().y >= navBarTopBuffer) {
+        // alert('Just scrolled past the top of the Bio Div! (from below)');
+        floatingNavBar.classList.remove('floating-nav-bar');
+    }
+
+    previousBioDivY = bioDiv.getBoundingClientRect().y;
+});
+
