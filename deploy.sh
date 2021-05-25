4536681e22971f70a707a1de17c464ecd6e23837
matt-harrop <osaintspreserveus@live.com>
2021-05-25 14:29:56 -0700

    Added '--noinput' flag to the collectstatic line of the deploy script to test if that is why some changes (like to CSS) aren't being deployed.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ sudo git pull origin main
 sudo pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
